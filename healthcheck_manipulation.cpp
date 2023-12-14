bd101cd2b1aefcd8814f31ae2f2507b4e4d32e0f
rafal-gorecki <rafal.gorecki@husarion.com>
2023-12-14 15:46:03 +0100

    Healthcheck fix

--- healthcheck_manipulation.cpp
+++ healthcheck_manipulation.cpp
@@ -6,8 +6,8 @@
 
 using namespace std::chrono_literals;
 
-#define LOOP_PERIOD 2s
-#define MSG_VALID_TIME 5s
+#define LOOP_PERIOD 500ms
+#define MSG_VALID_TIME 2s
 
 std::chrono::steady_clock::time_point last_odom_msg_time;
 std::chrono::steady_clock::time_point last_joint_msg_time;
@@ -53,8 +53,8 @@ void healthy_check() {
       current_time - last_odom_msg_time;
   std::chrono::duration<double> joint_elapsed_time =
       current_time - last_joint_msg_time;
-  bool is_odom_valid = odom_elapsed_time.count() < MSG_VALID_TIME.count();
-  bool are_joints_valid = joint_elapsed_time.count() < MSG_VALID_TIME.count();
+  bool is_odom_valid = odom_elapsed_time < MSG_VALID_TIME;
+  bool are_joints_valid = joint_elapsed_time < MSG_VALID_TIME;
 
   if (is_odom_valid && are_joints_valid) {
     write_health_status("healthy");
